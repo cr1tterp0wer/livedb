7ace1cc4a80b293c89d997a1227f7381454cbf45
cr1tterp0wer <capodacac@gmail.com>
2016-10-21 03:04:55 -0700

    initial commit

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php 
+//////// 
+// 
+// Get Data Server
+// @getTable:: The method arg
+// @return::   Returns a json result from
+//             the database 'filing' table
+//
+//Author: Christopher Stenqvist
+//Date:   10/21/2016
+// 
+//////// 
+
+if(isset($_GET['getTable'] )){
+	getTable();
+}
+
+function getTable(){
+	
+	$mysqli = new mysqli('localhost','root','','mama');
+	$jsonArray = array();
+	if ($result = $mysqli->query("SELECT * FROM filing")) {
+
+	    while($row = $result->fetch_array(MYSQL_ASSOC)) {
+	            $jsonArray[] = $row;
+	    }
+	    
+	}
+	$result->close();
+	$mysqli->close();
+	
+	echo json_encode($jsonArray);
+}
+
+
+?>
+
+
